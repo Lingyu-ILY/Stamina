e131e946ea0f5a88bbd608091896001aab10275f
Lingyu-ILY <59048645+Lingyu-ILY@users.noreply.github.com>
2020-09-30 17:37:43 +0800

    update

--- src/com/Lingyu/Main/Main.java
+++ src/com/Lingyu/Main/Main.java
@@ -1,17 +1,8 @@
 package com.Lingyu.Main;
 
-import net.md_5.bungee.api.ChatMessageType;
-import net.md_5.bungee.api.chat.TextComponent;
-
-import java.text.DecimalFormat;
-
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.event.*;
-import org.bukkit.event.entity.FoodLevelChangeEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
-import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.*;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -27,37 +18,24 @@ public class Main extends JavaPlugin implements Listener {
     // Fired when plugin is disabled
     @Override
     public void onDisable() {
-    	getServer().getConsoleSender().sendMessage("HungerLock is disable!");
+    	getServer().getConsoleSender().sendMessage("Stamina is disable!");
     }
-    
 
-public void startTimer(Plugin plugin)
-{
-    GameTimer timer = new GameTimer();
-    Bukkit.getServer().getScheduler().runTaskTimer(plugin, timer, 20, 20); // 20 ticks is 1 second
-}
- 
-private class GameTimer implements Runnable
-{
-    int remainingTime = 2; // Seconds
- 
-    @Override
-    public void run() {
-    	Player player = (Player) player.getPlayer();
-        if (remainingTime == 0)
-        {
-        	player.setFoodLevel(player.getFoodLevel() +1);
-        }
-        remainingTime -= 1;
-}
-}
-
-    public void onPlayerRun(PlayerMoveEvent event) {
-    	Player player = event.getPlayer();
-    	if(player.isSprinting()) {
-    		player.setFoodLevel(player.getFoodLevel() -1);
-    	}
-    	else
-			player.setFoodLevel(player.getFoodLevel() +1);
+    public void onJoin(PlayerJoinEvent event) {
+        Player p = event.getPlayer();
+        Bukkit.getScheduler().runTaskLaterAsynchronously(this, new Runnable() {
+           
+            @Override
+            public void run() {
+                while(p.isOnline()) {
+                    if(p.isSprinting()) {
+                    	p.setFoodLevel(p.getFoodLevel() -1);
+                    }
+                    else {
+						p.setFoodLevel(p.getFoodLevel() +1);
+					}
+                }
+            }
+        }, 40l);
     }
 }
